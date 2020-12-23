abbf0b35e83a05be065c51354d90ca2589098ddb
qnrjs42 <qnrjs42@naver.com>
2020-12-24 05:14:50 +0900

    2-6. Modules and Dependency Injection

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber, IsString } from 'class-validator';
 import { CreateMovieDto } from './create-movie.dto';
 
 // CreateMovieDto property 구조가 똑같다. 필수 사항 제외하면
